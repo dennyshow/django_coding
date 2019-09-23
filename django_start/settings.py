4c6f7a02d7c435f479da7749df7fe2d73b8deac7
EC2 Default User <ec2-user@ip-172-31-84-209.ec2.internal>
2019-09-23 02:18:45 +0000

    Remove default SECRET_KEY

--- django_start/settings.py
+++ django_start/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'ka^%np5-witqw$ug5m$y0z&(wss9bq6!hxq!@3o924zzr!__p6')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
